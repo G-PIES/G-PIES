99d04d914055213470970f1cb4055be515aa1dd5
ksnedeker1 <snedekek@oregonstate.edu>
2024-02-21 02:28:30 -0500

    Improve data accumulation efficiency

--- src/ui/gui/simulationworker.py
+++ src/ui/gui/simulationworker.py
@@ -14,16 +14,17 @@ class SimulationWorker(QObject):
         self.C = C
         self.simulation_time = simulation_time
         self.step = step
-        self.df = pd.DataFrame(columns =['Time (s)', 'Cluster Size', 'Interstitials / cm^3', 'Vacancies / cm^3'])
 
     def run(self):
         t = 0
         cd = pycd.Simulation(self.C, self.simulation_time, self.step)
+        data = []
         while t < self.simulation_time:
             cd.run()
-            for i in range(1,self.C):
-                self.df.loc[len(self.df)] = ([t, i, cd.get_int_idx(i), cd.get_vac_idx(i)])
-            
+            for i in range(1, self.C):
+                data.append([t, i, cd.get_int_idx(i), cd.get_vac_idx(i)])
+
             t = cd.get_state_time()
 
-        self.finished.emit(self.df)
+        df = pd.DataFrame(data, columns=['Time (s)', 'Cluster Size', 'Interstitials / cm^3', 'Vacancies / cm^3'])
+        self.finished.emit(df)
