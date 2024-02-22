a07025982f8741faf01f5d606ef25b80a114ee64
ComfyBenjo <benreed447@gmail.com>
2024-02-21 19:08:02 -0800

    Graph auto populates when simualation starts (slow?)

--- src/ui/gui/mainwindowcontroller.py
+++ src/ui/gui/mainwindowcontroller.py
@@ -2,11 +2,17 @@ from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtCore import QThread
 from gui.mainwindow import Ui_MainWindow
 from simulationworker import SimulationWorker
+
+from simulationprocess import SimulationProcess
 from mplcanvas import MplCanvas
+from multiprocessing import Process, Pipe
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import threading
+
+import numpy as np
 
 class MainWindowController(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
@@ -21,23 +27,49 @@ class MainWindowController(QMainWindow, Ui_MainWindow):
 
     def init_connections(self):
         self.pushButton_2.clicked.connect(self.start_simulation)
+        self.pushButton_3.clicked.connect(self.stop_simulation)
 
     def start_simulation(self):
-        self.thread = QThread()
-        self.worker = SimulationWorker(C=10, simulation_time=1.0, step=0.00001)
-        self.worker.moveToThread(self.thread)
-        self.thread.started.connect(self.worker.run)
-        self.worker.finished.connect(self.set_df)
-        self.worker.finished.connect(self.thread.quit)
-        self.worker.finished.connect(self.worker.deleteLater)
-        self.thread.finished.connect(self.thread.deleteLater)
+        self.parent_conn, child_conn = Pipe()
+        self.sim_process = SimulationProcess(C=10, simulation_time=1.0, step=0.00001, pipe=child_conn)
+
+        self.thread = threading.Thread(target=self.start_waiting_for_task)
         self.thread.start()
 
-    def init_graph(self):
-        self.df = pd.DataFrame(columns =['Time (s)', 'Cluster Size', 'Interstitials / cm^3', 'Vacancies / cm^3'])
-        self.sc = MplCanvas(self, width=5, height=4, dpi=100)
-        self.df.plot.scatter(ax=self.sc.axes, x = 'Time (s)', y = 'Interstitials / cm^3')
-        self.verticalLayout_2.addWidget(self.sc)
+        print("simulation started")
 
-    def set_df(self, result):
-        self.df = result
+    def stop_simulation(self):
+        if(self.sim_process.is_alive()):
+            self.sim_process.terminate()
+            self.parent_conn.close()
+        print("simulation stopped")
+
+    def init_graph(self):
+        #plt.
+        self._plot_ref = None
+        self.data_x = []
+        self.data_y = []
+        #self.df = pd.DataFrame(columns =['Time (s)', 'Cluster Size', 'Interstitials / cm^3', 'Vacancies / cm^3'])
+        self.scatter_plot = MplCanvas(self, width=5, height=4, dpi=100)
+        #self.df.plot.scatter(ax=self.sc.axes, x = 'Time (s)', y = 'Interstitials / cm^3')
+        self.scatter_plot.axes.set_xlabel('Time (s)')
+        self.scatter_plot.axes.set_ylabel('Interstitials / cm^3')
+        #self.scatter_plot.axes.scatter()
+        self.verticalLayout_2.addWidget(self.scatter_plot)
+        self.scatter_plot.show()
+
+    def start_waiting_for_task(self):
+        self.sim_process.start()
+        while(self.sim_process.is_alive()):
+            self.update_graph()
+        self.sim_process.join()
+        print("simulation finished")
+
+    def update_graph(self):
+        data = self.parent_conn.recv()
+        self.data_x.append(data[0])
+        self.data_y.append(data[2])
+        
+        self.scatter_plot.axes.cla()
+        self.scatter_plot.axes.scatter(x=self.data_x, y=self.data_y)
+        self.scatter_plot.draw()
