6ea836537aa54591091dd6fd8e88b9e342d753b7
ksnedeker1 <snedekek@oregonstate.edu>
2024-02-26 11:46:58 -0500

    Move

--- src/ui/gui/visualization/mplcanvas.py
+++ src/ui/gui/visualization/mplcanvas.py
@@ -1,11 +1,9 @@
-import sys
 import matplotlib
 matplotlib.use('Qt5Agg')
 
-import matplotlib.pyplot as plt
-
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
+
 
 class MplCanvas(FigureCanvasQTAgg):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
