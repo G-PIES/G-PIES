26d59fee8dc6633416f377dcc8ebac015febf423
Sean Siders <41221800+SeanSiders@users.noreply.github.com>
2024-03-04 19:06:46 -0800

    Remove unneeded csv functions

--- include/utils/blob_converter.hpp
+++ include/utils/blob_converter.hpp
@@ -22,30 +22,6 @@ class BlobConverter {
         std::memcpy(vec.data(), blob.data(), blob.size());
         return vec;
     }
-
-    static std::string to_csv(const std::vector<gp_float>& vec) {
-        std::stringstream ss;
-        size_t len = vec.size();
-        for (size_t i = 0; i < len - 1; ++i) {
-            ss << vec[i] << ',';
-        }
-        ss << vec[len - 1];
-
-        return ss.str();
-    }
-
-    static std::vector<gp_float> from_csv(const std::string& csv) {
-        std::stringstream ss(csv);
-        std::vector<gp_float> vec;
-
-        while (ss.good()) {
-            std::string str;
-            getline(ss, str, ',');
-            vec.push_back((gp_float)std::stod(str));
-        }
-
-        return vec;
-    }
 };
 
 #endif  // BLOB_CONVERTER_HPP
