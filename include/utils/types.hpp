b9b39a12dd0c432c13e2d7310868bf267ea48979
Sean Siders <41221800+SeanSiders@users.noreply.github.com>
2024-03-13 13:30:36 -0700

    Linting fix

--- include/utils/types.hpp
+++ include/utils/types.hpp
@@ -2,7 +2,7 @@
 #define TYPES_HPP
 
 #if defined(USE_METAL)
-// metal does not support doubles 
+// metal does not support doubles
 #define GP_FLOAT float
 #elif !defined(GP_FLOAT)
 #define GP_FLOAT double
