b2ac38ebea4e8d81e16b750552c1c8ec1495e021
Sean Siders <41221800+SeanSiders@users.noreply.github.com>
2024-03-13 13:57:23 -0700

    Rollback 9e328256447615bfe70b4034081d9c648141f31b

--- include/utils/types.hpp
+++ include/utils/types.hpp
@@ -1,10 +1,7 @@
 #ifndef TYPES_HPP
 #define TYPES_HPP
 
-#if defined(USE_METAL)
-// metal does not support doubles
-#define GP_FLOAT float
-#elif !defined(GP_FLOAT)
+#ifndef GP_FLOAT
 #define GP_FLOAT double
 #endif
 
