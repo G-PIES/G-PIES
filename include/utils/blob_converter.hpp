df0ba1e471c4a394ad22d5f08a32876d85bbad62
ksnedeker1 <snedekek@oregonstate.edu>
2024-04-18 21:48:13 -0400

    Add compression to blob serializer

--- include/utils/blob_converter.hpp
+++ include/utils/blob_converter.hpp
@@ -5,21 +5,48 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/filter/bzip2.hpp>
 
 #include "types.hpp"
 
-// TODO: Consider adding compression/decompression
+
 class BlobConverter {
- public:
-  static std::vector<char> to_blob(const std::vector<gp_float>& vec) {
-    std::vector<char> blob(vec.size() * sizeof(gp_float));
-    std::memcpy(blob.data(), vec.data(), blob.size());
+public:
+  static std::vector<char> to_blob(const std::vector <gp_float> &vec) {
+    if (vec.empty())
+      return {};
+
+    std::stringstream data;
+    data.write(reinterpret_cast<const char *>(vec.data()), vec.size() * sizeof(gp_float));
+
+    std::stringstream data_comp;
+    boost::iostreams::filtering_streambuf <boost::iostreams::output> out;
+    out.push(boost::iostreams::bzip2_compressor());
+    out.push(data_comp);
+    boost::iostreams::copy(data, out);
+
+    std::string str_comp = data_comp.str();
+    std::vector<char> blob(str_comp.begin(), str_comp.end());
     return blob;
   }
 
-  static std::vector<gp_float> from_blob(const std::vector<char>& blob) {
-    std::vector<gp_float> vec(blob.size() / sizeof(gp_float));
-    std::memcpy(vec.data(), blob.data(), blob.size());
+  static std::vector <gp_float> from_blob(const std::vector<char> &blob) {
+    if (blob.empty())
+      return {};
+
+    std::string str_comp(blob.begin(), blob.end());
+    std::stringstream data_comp(str_comp);
+
+    std::stringstream data;
+    boost::iostreams::filtering_streambuf <boost::iostreams::input> in;
+    in.push(boost::iostreams::bzip2_decompressor());
+    in.push(data_comp);
+    boost::iostreams::copy(in, data);
+
+    std::vector <gp_float> vec(data.str().size() / sizeof(gp_float));
+    data.read(reinterpret_cast<char *>(vec.data()), data.str().size());
     return vec;
   }
 };
