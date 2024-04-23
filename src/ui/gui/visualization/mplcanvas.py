97489d198f02fc8295b7f83abc9abf51f6f870bf
BenReed161 <benreed447@gmail.com>
2024-04-23 15:50:29 -0700

    new colors new design

--- src/ui/gui/visualization/mplcanvas.py
+++ src/ui/gui/visualization/mplcanvas.py
@@ -8,5 +8,18 @@ from matplotlib.figure import Figure
 class MplCanvas(FigureCanvasQTAgg):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
+        fig.patch.set_facecolor('#1d1d1d')
         self.axes = fig.add_subplot(111)
+        self.axes.set_facecolor('#1d1d1d')
+        self.axes.spines['top'].set_color("#76b800")
+        self.axes.spines['bottom'].set_color("#76b800")
+        self.axes.spines['left'].set_color("#76b800")
+        self.axes.spines['right'].set_color("#76b800")
+        self.axes.tick_params(axis='x', colors='#76b800')
+        self.axes.tick_params(axis='y', colors='#76b800')
+        [t.set_color('#76b800') for t in self.axes.xaxis.get_ticklabels()]
+        [t.set_color('#76b800') for t in self.axes.yaxis.get_ticklabels()]
+        self.axes.xaxis.label.set_color('#76b800')
+        self.axes.yaxis.label.set_color('#76b800')
+
         super(MplCanvas, self).__init__(fig)
