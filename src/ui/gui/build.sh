58417e841ef06a6d67cabddad548b633f14cefb6
BenReed161 <benreed447@gmail.com>
2024-04-23 18:43:45 -0700

    updated ui

--- src/ui/gui/build.sh
+++ src/ui/gui/build.sh
@@ -1,17 +1,20 @@
+printf "Running pyrcc5 .. compiling resources --"
 if pyrcc5 resources/resources.qrc -o resources_rc.py; then
-	printf "pyrcc5 ran correctly \n"
+	printf " success!\n"
 else
-	printf "something went wrong compiling python resource file."
+	printf " something went wrong running pyrcc5.\n"
 fi
 
+printf "Running pyuic5 .. compiling ui file --"
 if pyuic5 mainwindow.ui -o mainwindow.py; then
-	printf "pyuic5 ran correctly -- editing mainwindow.py\n"
+	printf " success!\n"
+	printf "Editing py file to correct import --"
 	if sed -i 's/import resources_rc/from .resources_rc import qInitResources/g' mainwindow.py; then
-		printf "edit success!\n"
+		printf " edit success!\n"
 	else
-		printf "something went wrong when running sed"
+		printf " something went wrong when running sed.\n"
 	fi
 
 else
-	printf "something went wrong running pyuic5"
+	printf " something went wrong running pyuic5.\n"
 fi
--- src/ui/gui/visualization/mplcanvas.py
+++ src/ui/gui/visualization/mplcanvas.py
@@ -11,15 +11,15 @@ class MplCanvas(FigureCanvasQTAgg):
         fig.patch.set_facecolor('#1d1d1d')
         self.axes = fig.add_subplot(111)
         self.axes.set_facecolor('#1d1d1d')
-        self.axes.spines['top'].set_color("#76b800")
-        self.axes.spines['bottom'].set_color("#76b800")
-        self.axes.spines['left'].set_color("#76b800")
-        self.axes.spines['right'].set_color("#76b800")
-        self.axes.tick_params(axis='x', colors='#76b800')
-        self.axes.tick_params(axis='y', colors='#76b800')
-        [t.set_color('#76b800') for t in self.axes.xaxis.get_ticklabels()]
-        [t.set_color('#76b800') for t in self.axes.yaxis.get_ticklabels()]
-        self.axes.xaxis.label.set_color('#76b800')
-        self.axes.yaxis.label.set_color('#76b800')
+        self.axes.spines['top'].set_color("#ffffff")
+        self.axes.spines['bottom'].set_color("#ffffff")
+        self.axes.spines['left'].set_color("#ffffff")
+        self.axes.spines['right'].set_color("#ffffff")
+        self.axes.tick_params(axis='x', colors='#ffffff')
+        self.axes.tick_params(axis='y', colors='#ffffff')
+        [t.set_color('#ffffff') for t in self.axes.xaxis.get_ticklabels()]
+        [t.set_color('#ffffff') for t in self.axes.yaxis.get_ticklabels()]
+        self.axes.xaxis.label.set_color('#ffffff')
+        self.axes.yaxis.label.set_color('#ffffff')
 
         super(MplCanvas, self).__init__(fig)
